ac92ece9fd2295da565eb58fab0f4ce5cf82ee64
USBMODS4DAYS <saket.nadimpalla@gmail.com>
2025-03-16 17:05:47 +1100

    Pseudocode
    Pseudocode for self_test and Calibration

--- prototyping/Calibration_Routine/src/main.cpp
+++ prototyping/Calibration_Routine/src/main.cpp
@@ -1,18 +1,51 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+void setup() 
+{
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void calibrateAccelerometer()
+{
+    DEFINE orientations = ["+X Up", "-X Up", "+Y Up", "-Y Up", "+Z Up", "-Z Up"]
+    DEFINE rawData = [xPlus, xMinus, yPlus, yMinus, zPlus, zMinus]
+
+    FOR each orientation in orientations DO
+        PRINT "Place sensor in position: ", orientation
+        WAIT 5 seconds  // Allow user time to position the sensor
+
+        SET sum = 0
+        FOR j FROM 1 TO NUM_SAMPLES DO
+            READ sensor values (X, Y, Z)
+            IF measuring X-axis THEN sum += X
+            ELSE IF measuring Y-axis THEN sum += Y
+            ELSE sum += Z
+            WAIT 10ms  // Small delay for stable readings
+        ENDFOR
+
+        STORE average sensor value in rawData
+    ENDFOR
+
+    // Calculate calibration values
+    OffsetX = (xPlus + xMinus) / 2
+    OffsetY = (yPlus + yMinus) / 2
+    OffsetZ = (zPlus + zMinus) / 2
+
+    ScaleX = (xPlus - xMinus) / 2
+    ScaleY = (yPlus - yMinus) / 2
+    ScaleZ = (zPlus - zMinus) / 2
 }
+
+FUNCTION getCalibratedAcceleration(OUTPUT Xg, Yg, Zg)
+    READ raw accelerometer values (Xraw, Yraw, Zraw)
+
+    Xg = (Xraw - OffsetX) / ScaleX
+    Yg = (Yraw - OffsetY) / ScaleY
+    Zg = (Zraw - OffsetZ) / ScaleZ
+
+    RETURN Xg, Yg, Zg  // Corrected acceleration values
--- prototyping/Self_Test_Routine/src/main.cpp
+++ prototyping/Self_Test_Routine/src/main.cpp
@@ -1,18 +1,68 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+void setup() 
+{
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+float read_voltage(int pin)
+{
+  READ analog value from pin
+  CONVERT to voltage using ADC formula
+  RETURN voltage
+}
+
+void self_test()
+{
+PRINT "Starting Self-Test..."
+
+// Step 1: Read baseline acceleration values
+x_baseline = read_voltage(X_PIN)
+y_baseline = read_voltage(Y_PIN)
+z_baseline = read_voltage(Z_PIN)
+
+// Step 2: Enable self-test mode
+SET ST_PIN HIGH
+WAIT 100 milliseconds
+
+// Step 3: Read new acceleration values
+x_test = read_voltage(X_PIN)
+y_test = read_voltage(Y_PIN)
+z_test = read_voltage(Z_PIN)
+
+// Step 4: Calculate changes
+x_change = x_test - x_baseline
+y_change = y_test - y_baseline
+z_change = z_test - z_baseline
+
+PRINT "X Change:", x_change
+PRINT "Y Change:", y_change
+PRINT "Z Change:", z_change
+
+// Step 5: Compare with expected values
+pass = TRUE
+IF absolute(x_change - (-0.325)) > 0.1 THEN
+    PRINT "X-axis test failed!"
+    pass = FALSE
+ENDIF
+
+IF absolute(y_change - (0.325)) > 0.1 THEN
+    PRINT "Y-axis test failed!"
+    pass = FALSE
+ENDIF
+
+IF absolute(z_change - (0.550)) > 0.1 THEN
+    PRINT "Z-axis test failed!"
+    pass = FALSE
+ENDIF
+
+// Step 6: Disable self-test mode
+SET ST_PIN LOW
+
+// Step 7: Output result
 }
